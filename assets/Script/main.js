dc0966b341881fa3e5981b6c4ef5dcc0f11526dc
yutou527 <642114645@qq.com>
2019-10-09 15:17:35 +0800

    build

--- assets/Script/main.js
+++ assets/Script/main.js
@@ -2,11 +2,15 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-       
+       testNode:cc.Node,
     },
 
     // use this for initialization
     onLoad: function () {
+
+        this.scheduleOnce(_ => { 
+            this.testNode.active = true;
+        },0.1)
     },
 
     // called every frame
