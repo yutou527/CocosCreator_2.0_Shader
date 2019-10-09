8bd9eb7d5b3a2d5c9b8bce502eedd8f71cfa8ad3
yutou527 <642114645@qq.com>
2019-10-09 15:08:13 +0800

    build

---
+++ build/web-mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],rawAssets:{assets:{}},assetTypes:[],launchScene:"db://assets/Scene/main.fire",scenes:[{url:"db://assets/Scene/main.fire",uuid:0}],packedAssets:{"0e2e3aef3":[0,"50j4pfRIFOsa1cs9cQm8gX"]},md5AssetsMap:{},orientation:"",subpackages:{},uuids:["2dL3kvpAxJu6GJ7RdqJG5J"]};
